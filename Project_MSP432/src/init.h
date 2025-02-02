5d4761d5382ea714bf083c81b4cc4549949a4fef
GiacomoSal <79629188+GiacomoSal@users.noreply.github.com>
2025-02-02 12:18:15 +0100

    Modified, now it works measuring the effective temperature

---
+++ Project_MSP432/src/init.h
@@ -0,0 +1,10 @@
+#ifndef __INIT_H__
+#define __INIT_H__
+
+void _graphicsInit();
+
+void _hwInit();
+
+void _showTextTemp(char *string, float temp);
+
+#endif
---
+++ Project_MSP432/src/uart.h
@@ -0,0 +1,8 @@
+#ifndef __UART_H__
+#define __UART_H__
+
+void _uartInit();
+
+void UART_sendString(char *str);
+
+#endif
