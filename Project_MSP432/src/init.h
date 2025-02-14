0f4e9b7c408e424da97e3e75cdc9775d00312ea7
GiacomoSal <79629188+GiacomoSal@users.noreply.github.com>
2025-02-14 16:21:30 +0100

    Added microphone modification

---
+++ Project_MSP432/src/init.h
@@ -0,0 +1,14 @@
+#ifndef __INIT_H__
+#define __INIT_H__
+
+void  _temperatureSensorInit();
+
+void _micInit();
+
+void _graphicsInit();
+
+void _hwInit();
+
+void _showText(char *title, char *text);
+
+#endif
